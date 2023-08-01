ddd8f4baf4d78b7ebc1fdc70d77eda82146d4a16
mohametalmeari <mohamet.almeari@gmail.com>
2023-08-01 19:23:00 +0300

    Test creating instance of Solver class

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,13 @@
 require_relative '../Solver'
 
-describe Solver do 
+describe Solver do
+  before :each do
+    @solver = Solver.new
+  end
 
+  context '#new' do
+    it 'takes no parameters and returns a Solver object' do
+      @solver.should be_an_instance_of Solver
+    end
+  end
 end
